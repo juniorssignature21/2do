78855263a8f9d2de90daa17dec205cf6662ca5f8
codesignature <codesignature19@gmail.com>
2025-10-26 16:24:07 +0100

    edited settings

--- proj/settings.py
+++ proj/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -97,6 +97,8 @@ DATABASES = {
         'PORT': config('PORT'),
     }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
